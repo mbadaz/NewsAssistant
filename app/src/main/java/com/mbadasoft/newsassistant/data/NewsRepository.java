7192f202ca18dbc5ca3cb09bf0ef771851debc64
mbadaz <tapsmuzira@gmail.com>
2019-10-02 21:03:29 +0200

    Added article list item layout

--- app/src/main/java/com/mbadasoft/newsassistant/data/NewsRepository.java
+++ app/src/main/java/com/mbadasoft/newsassistant/data/NewsRepository.java
@@ -1,6 +1,11 @@
 package com.mbadasoft.newsassistant.data;
 
+import com.mbadasoft.newsassistant.models.Article;
+import com.mbadasoft.newsassistant.models.Source;
+
+import java.util.List;
+
 public interface NewsRepository {
-
-
+    List<Source> getSources();
+    List<Article> getArticles();
 }
